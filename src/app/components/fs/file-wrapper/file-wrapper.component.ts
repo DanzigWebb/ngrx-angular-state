3aa7c0ccec82761c95499b781b21bb95c236ccc8
Alexandr <valverin95@gmail.com>
2020-12-15 20:44:19 +0300

    feat: add fs module

---
+++ src/app/components/fs/file-wrapper/file-wrapper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-file-wrapper',
+  templateUrl: './file-wrapper.component.html',
+  styleUrls: ['./file-wrapper.component.scss']
+})
+export class FileWrapperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/fs/fs.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FileItemComponent } from './file-item/file-item.component';
+import { FileWrapperComponent } from './file-wrapper/file-wrapper.component';
+
+
+@NgModule({
+  declarations: [
+    FileItemComponent,
+    FileWrapperComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FsModule { }
