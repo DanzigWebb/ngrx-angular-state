e4440d16aa02bcac61eae524b0571eea8c199040
Alexandr <valverin95@gmail.com>
2020-12-27 14:24:37 +0300

    style: appComp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppLoaderService } from '@app/shared/service/app-loader.service';
-import { debounceTime, distinctUntilChanged, tap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -18,7 +18,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.loader.isLoading$.pipe(
-      tap(isLoad => console.log(isLoad)),
       debounceTime(100),
       distinctUntilChanged()
     ).subscribe(state => {
